fd4d2781cf91f6c5a01fbc8f1b5ca2e788dc6120
hymced <hymced@gmail.com>
2025-08-13 18:11:13 +0200

    add global styles

--- apps/form/src/main.tsx
+++ apps/form/src/main.tsx
@@ -31,8 +31,8 @@ const StyledButton = styled("button", {
 })
 
 const App = () => (
-  <>
-    <div>
+  <div>
+    {/* <div>
       <a href="https://vitejs.dev" target="_blank">
         <img src="/vite.svg" className="logo" alt="Vite logo" />
       </a>
@@ -43,14 +43,19 @@ const App = () => (
           alt="TypeScript logo"
         />
       </a>
-    </div>
+    </div> */}
     <Counter />
     <StyledButton variant="primary" onClick={() => console.log("hello")}>primary</StyledButton>
     <StyledButton variant="secondary" onClick={() => console.log("hello")}>secondary</StyledButton>
     <button className={css({color: "cobalt"})}>color cobalt</button>
     <button className={css({color: "kale"})}>color kale</button>
     <button className={css({bg: "red"})}>bg red</button>
-  </>
+    <button className={css({ foo: 'lg', border: "1px solid red" })}>foo lg</button>
+    <styled.button foo="sm" border="1px solid red">foo sm</styled.button>
+    <button className={css({barX: "2px", _hover: {bg: "yellow"}})}>barX _hover</button>
+    <styled.button bx="2px">bx</styled.button>
+    <button className={css({barX: "2px", _hover: {color: "red"}})}>safe hover</button>
+  </div>
 );
 
 createRoot(document.getElementById("app")!).render(<App />);
